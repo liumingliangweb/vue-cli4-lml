66b349dcc106bb6ee60e908955ba037aa4d8c04b
liumingliangweb <lmllml2009@163.com>
2020-11-04 11:19:00 +0800

    路由功能完善

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,29 @@ Vue.use(Router)
 
 const createRouter = () =>
   new Router({
-    // mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
-    // base: process.env.BASE_URL,
-    scrollBehavior: () => ({ y: 0 }),
-    routes: constantRouterMap
+    mode: 'history', // 如果你是 history模式 需要配置vue.config.js publicPath
+    base: process.env.BASE_URL, // 如果你是 history模式 需要配置此项
+    scrollBehavior: () => ({ y: 0 }), // 路由改变，页面回到顶部
+    routes: constantRouterMap // 对应的路由数据
   })
 
 const router = createRouter()
+// 导航守卫-全局前置守卫
+router.beforeEach((to, from, next) => {
+  /* 必须调用 `next` */
+  next()
+  // next(false) 中断当前导航
+  // next('/') 跳转到相应路由
+})
 
+// router.beforeResolve((to, from, next) => {
+//   /* 必须调用 `next` */
+//   next()
+// })
+// 全局后置钩子
+router.afterEach((to, from) => {
+  // 页面跳转后的操作
+})
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
--- vue.config.js
+++ vue.config.js
@@ -38,8 +38,8 @@ const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
 // }
 
 module.exports = {
-  publicPath: './', // 署应用包时的基本 URL。 vue-router hash 模式使用
-  //  publicPath: '/app/', //署应用包时的基本 URL。  vue-router history模式使用
+  // publicPath: './', // 署应用包时的基本 URL。 vue-router hash 模式使用
+  publicPath: '/app/', // 署应用包时的基本 URL。  vue-router history模式使用
   outputDir: 'dist', //  生产环境构建文件的目录
   assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: !IS_PROD,
@@ -166,7 +166,7 @@ module.exports = {
             chunks: 'initial', // 只打包初始时依赖的第三方
             test: /[\\/]node_modules[\\/]/,
             priority: 10
-          },
+          }
           // vantUI: {
           //   name: 'chunk-vantUI', // 单独将 vantUI 拆包
           //   priority: 20, // 数字大权重到，满足多个 cacheGroups 的条件时候分到权重高的
